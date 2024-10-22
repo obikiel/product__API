9550898460a58f241a773e55e2a7eb89a9c3d0a0
obikiel <ezekielobiomachi1@gmail.com>
2024-10-22 19:44:08 +0100

    allowed host

--- Product_manager/settings.py
+++ Product_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-n-+l=^kx%5+tj4t7hl&)(*r8194wg6$gxyecb*@o1ro*d4tv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
